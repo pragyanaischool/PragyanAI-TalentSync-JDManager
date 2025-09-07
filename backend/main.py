dfb2d842f928851f175a7dc5894f1b2605398029
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-09-07 12:37:12 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api import jd_router
+#from app.api import jd_router
+from .app.api import jd_router
 
 # Create the FastAPI application instance
 app = FastAPI(
