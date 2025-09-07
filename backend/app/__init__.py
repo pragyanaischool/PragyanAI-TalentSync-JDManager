1758266def4014e22c00d562930e038fdca69a74
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2025-09-07 09:10:12 +0530

    Create __init__.py

---
+++ backend/app/__init__.py
@@ -0,0 +1,3 @@
+# This file can be left empty.
+# Its presence tells Python that the 'app' directory should be treated as a package,
+# which allows you to import modules from it elsewhere in your project.
